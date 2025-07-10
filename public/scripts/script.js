5c855263ca82de7cea25b6465864dd9c765053de
David Koloski <davidkoloski30@gmail.com>
2025-07-10 16:57:01 +0200

    Cenovnik is done, div under header is created and other things got reconstructed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,3 +2,8 @@ var x;
 function tab(){
      x = window.open("https://github.com/didy13/compfirstaid", "", "");
 }
+function disableDropdown(){
+     if(window.innerWidth < 991){
+          document.getElementById("dropdown").setAttribute("data-bs-toggle", "dropdown");
+     }
+}
